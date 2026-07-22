c6f51d15e9c0a26587795151076246d6ddad6854
Jeremy Bennett <jeremy.bennett@embecosm.com>
2026-07-22 17:43:48 +0100

    All files for basic AoT support for CORE-V.
    	* CMakeLists.txt: Add COREV target
    
    backends/corev/
    
    	* corev_backend.py: Created.
    	* corev_partitioner.py: Created.
    	* corev_quantizer.py: Created.
    
    examples/corev/
    
    	* aot_corev_compiler.py: Created.
    	* models.py: Created.
    
    tools/
    
    	* cmake/preset/default.cmake: Add COREV target
    
    Signed-off-by: Jeremy Bennett <jeremy.bennett@embecosm.com>

---
+++ backends/corev/corev_backend.py
@@ -0,0 +1,55 @@
+# CORE-V backend main program
+
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# Copyright (C) 2026 Embecosm Limited
+# Contributor Jeremy Bennett <jeremy.bennett@embecosm.com>
+# Contributor Shane Slattery <shane.slattery@embecosm.com>
+
+# This file is part of the Embecosm ExecuTorch tutorial for RISC-V.
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import logging
+from typing import final, List, Optional
+
+from executorch.exir.backend.backend_details import (
+    BackendDetails,
+    ExportedProgram,
+    PreprocessResult,
+)
+from executorch.exir.backend.compile_spec_schema import CompileSpec
+
+def generate_corev_compile_spec(
+    system_config: Optional[str] = None,
+    operators_to_skip: Optional[List[str]] = None,
+    extra_flags: Optional[str] = None,
+) -> List[CompileSpec]:
+    compile_spec: List[CompileSpec] = []
+    compiler_flags = []
+    output_format = None
+
+    if system_config is not None:
+        compile_spec.append(CompileSpec("system_config",
+                                        system_config.encode()))
+
+    # TODO: Likely broken, a List needs to be encoded properly.
+    if operators_to_skip is not None:
+        compile_spec.append(CompileSpec("operators_to_skip",
+                                        operators_to_skip.encode()))
+
+    if extra_flags is not None:
+        compile_spec.append(CompileSpec("extra_flags", extra_flags.encode()))
+
+    return compile_spec
+
+
+@final
+class CoreVBackend(BackendDetails):
+    @staticmethod
+    def preprocess(
+        edge_program: ExportedProgram,
+        compile_spec: List[CompileSpec],
+    ) -> PreprocessResult:
+        logging.info("CoreVBackend::preprocess")
+        binary = bytes()
+        return PreprocessResult(processed_bytes=binary)
